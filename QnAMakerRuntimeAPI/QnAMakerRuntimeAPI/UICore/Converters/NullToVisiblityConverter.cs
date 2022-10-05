6cc498518bd14f0af8b56975823f118bc96993ba
Patrick King <pk2197@outlook.com>
2022-10-05 16:16:11 -0400

    Update to .NET 6

--- QnAMakerRuntimeAPI/QnAMakerRuntimeAPI/UICore/Converters/NullToVisiblityConverter.cs
+++ QnAMakerRuntimeAPI/QnAMakerRuntimeAPI/UICore/Converters/NullToVisiblityConverter.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Globalization;
-using System.Runtime.InteropServices.WindowsRuntime;
 using System.Text;
 using System.Windows;
 using System.Windows.Data;

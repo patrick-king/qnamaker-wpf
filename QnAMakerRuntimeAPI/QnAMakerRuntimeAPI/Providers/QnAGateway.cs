84fdeb3666ae1e8a3bdcaa996a1f21d6d44f7151
patrick-king <33234942+patrick-king@users.noreply.github.com>
2022-10-06 11:47:03 -0400

    Update to custom question answering (#2)
    * Update to .NET 6
    
    * Complete dependency upgrades to .NET 6, connect to Language Service Custom Question Answering

--- QnAMakerRuntimeAPI/QnAMakerRuntimeAPI/Providers/QnAGateway.cs
+++ QnAMakerRuntimeAPI/QnAMakerRuntimeAPI/Providers/QnAGateway.cs
@@ -3,10 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Text;
-using Microsoft.Azure.CognitiveServices.Knowledge.QnAMaker;
-using Microsoft.Azure.CognitiveServices.Knowledge.QnAMaker.Models;
 using Microsoft.Extensions.Configuration;
-using Newtonsoft.Json;
+using Azure.AI.Language.QuestionAnswering;
+using Azure.Core;
+using Azure;
 
 namespace QnAMakerRuntimeAPI.Providers
 {
@@ -18,32 +18,34 @@ namespace QnAMakerRuntimeAPI.Providers
             _configuration = config;
         }
 
-        public async Task<QnASearchResultList> AnswerQuestion(string question, string userId, int resultCount = 1)
+        public async Task<AnswersResult> AnswerQuestion(string question, string userId, int resultCount = 1)
         {
-            //From QnA Knowledgebase - embedded GUID in POST portion
-            var runtimeKB = _configuration["QNA_KB_ID"];
-            //From QnA Knowledgebase - HOST
-            var runtimeEndpoint = _configuration["QNA_RuntimeEndpoint"];
-            //From QnA Knowledgebase - Authorization Endpoint Key
-            var runtimeKey = _configuration["QNA_RuntimeKey"];
+            //From the Deploy Knowledge Base page in Language Studio - Prediction URL, but drop everything after azure.com becuase 
+            //the SDK will supply those settings. Your url should look like this when using the SDK: https://<My Language Service>.cognitiveservices.azure.com"
+            var runtimeEndpoint = new Uri(_configuration["QuestionAnswering.Endpoint"]);
+            //From Azure Portal Language resource - Keys and Endpoint, pick one of the keys.
+            var runtimeKey = _configuration["QuestionAnswering.APIKey"];
+            //From the Deploy Knowledge Base page in Language Studio (Can extract projectName and DeploymentName from the example Prediction URL)
+            string projectName = _configuration["QuestionAnswering.ProjectName"];
+            string deploymentName = _configuration["QuestionAnswering.DeploymentName"];
 
-            var runtimeCreds = new EndpointKeyServiceClientCredentials(runtimeKey);
+            var runtimeCreds = new AzureKeyCredential(runtimeKey);
 
             //Runtime client for asking questions
-            using (QnAMakerRuntimeClient rtClient = new QnAMakerRuntimeClient(runtimeCreds) { 
-                RuntimeEndpoint = runtimeEndpoint 
-            })
+            QuestionAnsweringClient rtClient = new QuestionAnsweringClient(runtimeEndpoint,runtimeCreds);
+
+            AnswersOptions options = new AnswersOptions()
             {
-                QueryDTO query = new QueryDTO()
-                {
-                    Question = question, //The text of the question
-                    ScoreThreshold = 0.55, //The minimum relevance score to accept
-                    Top = resultCount //How many answers to return, maximum
-                };
+                ConfidenceThreshold = 0.39,//The minimum relevance score to accept
+                Size = resultCount//How many answers to return, maximum
+            };
+              
+          
+            QuestionAnsweringProject project = new QuestionAnsweringProject(projectName, deploymentName);
 
-               return await rtClient.Runtime.GenerateAnswerAsync(runtimeKB, query).ConfigureAwait(false);
-
-            }
+         
+            return await rtClient.GetAnswersAsync(question, project, options);
+         
 
         }
 
